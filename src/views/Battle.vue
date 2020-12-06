9e6006921e1d95c35b45e7488f7219157e855150
CHooooCLate <kintaro_9161114_1@yahoo.co.jp>
2020-12-06 21:50:34 +0900

    [update] 一通りの処理

--- src/views/Battle.vue
+++ src/views/Battle.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="regist-player">
-    <form name="timer">
-      <p>{{ timer }}</p>
-      <input type="button" value="スタート" v-on:click="countStart()">
+    <form name="first-timer">
+      <p>{{ firstTimer }}</p>
+      <input type="button" value="スタート" v-on:click="countStartFirst()">
       <input type="button" value="ストップ" v-on:click="countStop()">
-      <input type="button" value="次へ">
+      <input type="button" value="次へ" v-on:click="nextStage()">
+    </form>
+    <form name="second-timer">
+      <p>{{ secondTimer }}</p>
+      <input type="button" value="スタート" v-on:click="countStartSecond()">
+      <input type="button" value="ストップ" v-on:click="countStop()">
     </form>
   </div>
 </template>
@@ -14,25 +19,44 @@ export default {
   name: 'Battle',
   data() {
     return {
-      timer: 10,
+      constTime: 10,
+      firstTimer: 0,
+      secondTimer: 0,
       timerObj: null,
     };
   },
+  mounted() {
+    this.firstTimer = this.constTime;
+  },
   methods: {
-    showPassage() {
-      if (this.timer > 0) {
-        this.timer--;
+    showFirstPassage() {
+      if (this.firstTimer > 0) {
+        this.firstTimer--;
       } else {
         alert("You're Loser...");
         clearInterval(this.timerObj);
       }
     },
-    countStart() {
-      this.timerObj = setInterval(this.showPassage, 1000);
+    countStartFirst() {
+      this.timerObj = setInterval(this.showFirstPassage, 1000);
     },
     countStop() {
       clearInterval(this.timerObj);
-    }
+    },
+    nextStage() {
+      this.secondTimer = this.constTime - this.firstTimer;
+    },
+    showSecondPassage() {
+      if (this.secondTimer > 0) {
+        this.secondTimer--;
+      } else {
+        alert("Vote!");
+        clearInterval(this.timerObj);
+      }
+    },
+    countStartSecond() {
+      this.timerObj = setInterval(this.showSecondPassage, 1000);
+    },
   }
 }
 </script>
