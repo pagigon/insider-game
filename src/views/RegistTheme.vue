f91605cc274cb04eae986110c6f8750483d788b7
CHooooCLate <kintaro_9161114_1@yahoo.co.jp>
2020-12-08 22:23:16 +0900

    [add] テーマ追加ページを追加

---
+++ src/views/RegistTheme.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="regist-player">
+    <label>追加するワード : </label>
+    <input v-model="word" placeholder="Word">
+    <button class="btn btn-primary" v-on:click="regist()">送信</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'RegistTheme',
+  data() {
+    return {
+      word: ''
+    }
+  },
+  methods: {
+    async regist() {
+      await axios
+        .post(
+          "https://v11izvxekf.execute-api.ap-northeast-1.amazonaws.com/prod/addTheme",
+          {
+            theme: this.word
+          }
+        )
+        .then(res => {
+          alert(res.data.body.id + "番目に追加されました！");
+        })
+        .catch(() => {
+          alert("通信に失敗しました。");
+        });
+    }
+  }
+}
+</script>
